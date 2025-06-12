251b81d6473a63f6a0e7ed3d43dc1e7949f6edd9
Eduardo <74874724+eduardotrj@users.noreply.github.com>
2025-06-12 16:06:28 +0100

    Add OpenAI connection async.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from openai import OpenAI
+from core.tasks import handle_ai_request_job
 
 
 # Create your models here.
@@ -33,3 +35,30 @@ class AiRequest(models.Model):
     response = models.JSONField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def _queue_job(self):
+        """Add job to queue."""
+        handle_ai_request_job.delay(self.id)
+
+    def handle(self):
+        """Handle request."""
+        self.status = self.RUNNING
+        self.save()
+        client = OpenAI()
+        try:
+            completion = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=self.messages,
+            )
+            self.response = completion.to_dict()
+            self.status = self.COMPLETE
+        except Exception:
+            self.status = self.FAILED
+
+        self.save()
+
+    def save(self, **kwargs):
+        is_new = self._state.adding
+        super().save(**kwargs)
+        if is_new:
+            self._queue_job()
